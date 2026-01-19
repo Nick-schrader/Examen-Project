1fc05925d75da906fa4989efb501fba3d9ccaa43
donie-banana <dion.gierman@gmail.com>
2026-01-19 12:44:23 +0100

    conflicts fixed that were unmarked by git

--- Brooklyn_sign-up/app/Models/Auto.php
+++ Brooklyn_sign-up/app/Models/Auto.php
@@ -10,23 +10,13 @@ class Auto extends Model
     use HasFactory;
 
     protected $table = 'auto';
-<<<<<<< HEAD
-=======
-    
-    public $timestamps = false;
->>>>>>> 303847d0f4a535bc8a210635339fd5905cc97fd0
 
     protected $fillable = [
         'kenteken',
         'merk',
         'type',
         'beschikbaar',
-<<<<<<< HEAD
-        'created_at',
-        'updated_at',
-=======
         'foto',
->>>>>>> 303847d0f4a535bc8a210635339fd5905cc97fd0
     ];
 
     public function roosterItems()
