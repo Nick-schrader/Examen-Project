1d12434bdf29f0044f10ab723d09cb2ddb8898d8
donie-banana <dion.gierman@gmail.com>
2026-01-19 13:29:41 +0100

    tabs added

--- Brooklyn_sign-up/database/seeders/DatabaseSeeder.php
+++ Brooklyn_sign-up/database/seeders/DatabaseSeeder.php
@@ -12,58 +12,58 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-    $faker = \Faker\Factory::create();
+        $faker = \Faker\Factory::create();
 
-    // Users
-    for ($i = 0; $i < 10; $i++) {
-        \App\Models\User::create([
-            'naam' => $faker->name,
-            'email' => $faker->unique()->safeEmail,
-            'password' => Hash::make($faker->password),
-            'telefoon' => $faker->phoneNumber,
-            'type' => $faker->numberBetween(1, 3),
-            'geboorte_datum' => $faker->date('Y-m-d', '2010-01-01'),
-            'geslacht' => $faker->randomElement(['M', 'V']),
-            'adres' => $faker->address,
-            'auto_preference' => null,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-    }
+        // Users
+        for ($i = 0; $i < 10; $i++) {
+            \App\Models\User::create([
+                'naam' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => Hash::make($faker->password),
+                'telefoon' => $faker->phoneNumber,
+                'type' => $faker->numberBetween(1, 3),
+                'geboorte_datum' => $faker->date('Y-m-d', '2010-01-01'),
+                'geslacht' => $faker->randomElement(['M', 'V']),
+                'adres' => $faker->address,
+                'auto_preference' => null,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
 
-    // Autos
-    for ($i = 0; $i < 5; $i++) {
-        \App\Models\Auto::create([
-            'kenteken' => strtoupper($faker->bothify('??-###-?')),
-            'merk' => $faker->company,
-            'type' => $faker->numberBetween(1, 3),
-            'beschikbaar' => $faker->boolean,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-    }
+        // Autos
+        for ($i = 0; $i < 5; $i++) {
+            \App\Models\Auto::create([
+                'kenteken' => strtoupper($faker->bothify('??-###-?')),
+                'merk' => $faker->company,
+                'type' => $faker->numberBetween(1, 3),
+                'beschikbaar' => $faker->boolean,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
 
-    // strippenkaart
-    for ($i = 0; $i < 5; $i++) {
-        \App\Models\Strippenkaart::create([
-            'leerling_id' => $faker->numberBetween(1, 10),
-            'tegoed' => $faker->numberBetween(1, 20),
-            'verval_datum' => Carbon::now()->addMonths($faker->numberBetween(1, 12)),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-    }
+        // strippenkaart
+        for ($i = 0; $i < 5; $i++) {
+            \App\Models\Strippenkaart::create([
+                'leerling_id' => $faker->numberBetween(1, 10),
+                'tegoed' => $faker->numberBetween(1, 20),
+                'verval_datum' => Carbon::now()->addMonths($faker->numberBetween(1, 12)),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
 
-    // Kortingen
-    for ($i = 0; $i < 5; $i++) {
-        \App\Models\Korting::create([
-            'leerling_id' => $faker->numberBetween(1, 10),
-            'percentage' => $faker->numberBetween(5, 50),
-            'reason' => $faker->sentence,
-            'is_used' => $faker->boolean,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-    }
+        // Kortingen
+        for ($i = 0; $i < 5; $i++) {
+            \App\Models\Korting::create([
+                'leerling_id' => $faker->numberBetween(1, 10),
+                'percentage' => $faker->numberBetween(5, 50),
+                'reason' => $faker->sentence,
+                'is_used' => $faker->boolean,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
