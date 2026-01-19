0de3024bd2413c168a37f79f69ff10baa9943aa4
Julian Huis in 't Veld <151150455+JulianHV@users.noreply.github.com>
2026-01-19 14:35:17 +0100

    vite config herstelt

--- Brooklyn_sign-up/vite.config.js
+++ Brooklyn_sign-up/vite.config.js
@@ -8,5 +8,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        }
+    },
 });
  
